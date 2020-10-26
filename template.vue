cf145d42f385116ffbc2b13303b5805a73feb569
rainbow <info@rainbow23.jp>
2020-10-26 23:26:41 +0900

    add samples

---
+++ template.vue
@@ -0,0 +1,19 @@
+<template>
+    <p>{{message}}!</p>
+</template>
+
+<script>
+export default = {
+    data () {
+        return {
+            message: 'こんにちは'
+        }
+    }
+}
+</script>
+
+<style scoped>
+p {
+    color: red;
+}
+</style>
